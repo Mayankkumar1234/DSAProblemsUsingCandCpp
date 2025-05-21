9131a6d31e6143a59e900cb9efc6044ffe27d1f3
Mayankkumar1234 <mk1070355@gmail.com>
2025-05-21 23:27:37 +0530

    second Problem

--- LinkedList/LinkedList.c
+++ LinkedList/LinkedList.c
@@ -20,7 +20,20 @@ struct Node *insertAtBeggining(struct Node *head, int data)
   ptr->next = head;
   return ptr;
 }
-
+// Insert A new node at the particular index...
+struct Node* insertInBetween(struct Node* head , int index, int data){
+          struct Node *ptr = (struct Node*)malloc(sizeof(struct Node));
+          struct Node *prev = head;
+         struct Node*temp = head;
+      for(int i=0;i<index;i++){
+           prev = temp;
+            temp = temp->next;   
+                           }      
+         prev->next = ptr;
+          ptr->data = data;
+          ptr->next = temp;                 
+       return head;
+            }
 void traverseLinkedList(struct Node *head)
 {
   struct Node *ptr = head;
@@ -54,6 +67,9 @@ int main()
   traverseLinkedList(head);
   head = insertAtBeggining(head, 20);
   printf("After inserting at the beginning\n");
+  traverseLinkedList(head);
+  head = insertInBetween(head, 2, 25);
+  printf("After inserting in between\n");
   traverseLinkedList(head);
   return 0;
 }
